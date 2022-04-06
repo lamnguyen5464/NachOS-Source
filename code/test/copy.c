eb2aa339a47e8340e73e61f18f5acb129ad125b3
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2022-04-07 06:55:55 +0700

    Dev: copy.c

--- code/test/copy.c
+++ code/test/copy.c
@@ -9,7 +9,6 @@
 
 int main(int argc, char *argv[0])
 {
-
 	char fileName1[100];
 	char fileName2[100];
 	int size = 10000;
@@ -19,20 +18,37 @@ int main(int argc, char *argv[0])
 
 	fileName1 = argv[0];
 	fileName2 = argv[1];
-		
 
-	if (fileName != NULL) {
-		fileId = Open(fileName);
-
-		if (fileId != -1) {
+	if (fileName1 != NULL)
+	{
+		fileId1 = Open(fileName1);
+		if (fileId1 != -1)
+		{
 			fileLength = Read(buffer, size, fileId1);
 			buffer[fileLength] = '\0';
-			PrintString(buffer);
-			Close(fileId);
+			if (fileName2 != NULL) {
+				fileId2 = Open(fileName2);
+			} else {
+				Createfile(fileName2);
+				fileId2 = Open(fileName2);
+			}
+			if (fileId2 != NULL)
+			{
+				Seek(0, fileId2);
+				Write(buffer, fileLength, fileId2);
+				Close(fileId2);
+			}
+			Close(fileId1);
 		}
-
+		else
+		{
+			PrintString("Can not open your file.");
+		}
 	}
-	PrintString("Enter fileName (enter '0' to stop): ");
+	else
+	{
+		PrintString("Your file does not exist.");
+	}
 
-  	Halt();
+	Halt();
 }
